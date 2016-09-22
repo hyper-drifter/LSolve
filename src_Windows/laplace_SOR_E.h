2d9c7a62894e01035b95f1e6c866ea1c6400907a
Taylor Grubbs <tedwingrubbs@gmail.com>
2016-09-21 22:55:02 -0400

    Added Electric Field calculation finally!!!
    :D

--- src_Windows/laplace_SOR_E.h
+++ src_Windows/laplace_SOR_E.h
@@ -38,7 +38,6 @@ relaxation method. Will incorporate dynamic over-relaxation
 */
 void laplaceSolve(double ** initPotGrid, int rows, int columns)
 {
-
      int i,j;
      int boundaryPoints[rows][columns]; //finds initial nonzero voltages (the electrodes)
      for(i = 0; i < rows; i++){
@@ -62,7 +61,7 @@ void laplaceSolve(double ** initPotGrid, int rows, int columns)
           deltaV = 0;
           for(i = 0; i < rows; i++){
                for(j = 0; j < columns; j++){
-                    if(boundaryPoints[i][j] == 1){continue;}
+                    if(boundaryPoints[i][j] == 1){continue;}//checks for boundary conditions
                     oldGridValues = newV[i][j];
 
                     //checks for every edge case
@@ -88,7 +87,7 @@ void laplaceSolve(double ** initPotGrid, int rows, int columns)
      printf("Solved in %i iterations.\n", iterNum); //prints number of iterations
 
      //writes new grid values to file
-     FILE *gridFile = fopen("LSolve-output.tsv", "w");
+     FILE *gridFile = fopen("LSolve-V-output.tsv", "w");
      for(i = 0; i < rows; i++){
        for(j = 0; j < columns; j++){
          fprintf(gridFile, " %lf\t", newV[i][j]);
@@ -97,5 +96,31 @@ void laplaceSolve(double ** initPotGrid, int rows, int columns)
      }
      fclose(gridFile);
 
-     printf("File saved as \"LSolve-output.tsv\"\n");
+     printf("File saved as \"LSolve-V-output.tsv\"\n");
+
+
+
+     /*
+     Calculates electric field from slope of potential.
+     This part may be a little hard to follow. From the beginning, I chose to think of the potential grid as a matrix with i rows and j columns. Conventional matrix notation denotes an entry with the row first and column second --> (i,j). This is backwards from normal physics coordinate systems which denote column first and row second --> (x,y). And so to return the list of electric field vectors in my grid, I have seemingly 'reversed' the placing of i and j here in order to return the field vectors with coordinates in traditional (x,y)-style.
+     */
+     double eField[rows-2][columns-2][2];
+
+     for (i = 1; i < rows-1; i++) { //das loop
+          for(j = 1; j < columns; j++){
+               if(boundaryPoints[i][j] == 1){continue;}
+               eField[i][j][1] = -(newV[i][j+1]-newV[i][j-1])/2.; //Ex
+               eField[i][j][2] = -(newV[i+1][j]-newV[i-1][j])/2.; //Ey
+          }
+     }
+
+     FILE *electricGridFile = fopen("LSolve-E-Output.tsv", "w");
+     for(i = 1; i < rows-1; i++){
+       for(j = 1; j < columns-1; j++){
+         fprintf(electricGridFile, "%i\t%i\t%lf\t%lf\n", j, i, eField[i][j][1], eField[i][j][2]);
+       }
+     }
+     fclose(electricGridFile);
+
+     printf("Electric fields saved as \"LSolve-E-output.tsv\"\n");
 }
